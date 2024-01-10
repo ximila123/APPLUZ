af9fe5d5051c5e197c19473db4e9803de476d691
ximila123 <155857880+ximila123@users.noreply.github.com>
2024-01-09 22:32:40 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Temblor en Perú hoy, según el IGP: últimos sismos del martes 9 de enero",
+"autor" => "El Comercio",
+"resumen" => "Sigue en vivo todos los movimientos telúricos en el país, según los reportes que publica el Instituto Geofísico del Perú.
+También consulta sobre noticias relacionadas, así como recomendaciones en este tipo de situaciones.",
 ];
 ?>
