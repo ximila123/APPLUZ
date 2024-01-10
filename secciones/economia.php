11676d2ad9f77d7b70ae36d030d32a68f3c47597
ximila123 <155857880+ximila123@users.noreply.github.com>
2024-01-09 22:19:11 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La economía peruana crecerá un 3.3% en el 2024",
+"autor" => "La Republica",
+"resumen" => "PBI crecería un 3.3% en el 2024 por las obras de infraestructura ",
 ];
 ?>
