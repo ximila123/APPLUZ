5961f6eb9f0ab9fc36fce0a01e72f140f1874c32
ximila123 <155857880+ximila123@users.noreply.github.com>
2024-01-09 22:35:57 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Felipe Quelopana: “Universitario y la vida después de Quispe y Fossati”",
+"autor" => "Perú 21",
+"resumen" => "«Hoy no ha venido nadie a preguntar; ni me han pedido en esta tarde nada», 
+pero de todas formas vengo a comentarles lo que se espera sobre la primera Noche Crema, la del Poeta.",
 ];
 ?>
