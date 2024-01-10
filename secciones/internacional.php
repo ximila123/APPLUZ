7f8c96df6183a5b85e8df0f27b6252211810b1df
ximila123 <155857880+ximila123@users.noreply.github.com>
2024-01-09 22:21:56 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,11 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Otra señal para Palestina: Chile irá a la Corte Penal Internacional a apoyar demandas contra Israel",
+"autor" => "La Tercera",
+"resumen" => "Durante la tarde de este martes, Paula Narváez, embajadora ante las Naciones Unidas,
+anunció en la Asamblea General de la organización que Chile hará una presentación en la Corte Penal Internacional (CPI)
+para que se investiguen la situación de Palestina en Gaza, apoyando así la decisión de Sudáfrica -y a otros países como Argelia,
+Bolivia, Bangladesh, Comoros, Colombia, Djibouti y Turquía- de presentar o apoyar denuncias en el organismo internacional por “genocidio”",
 ];
 ?>
